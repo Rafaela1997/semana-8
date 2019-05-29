4e0ced84c90cbc716cf304b8687ac1a016e8f772
rafaela1997 <rafaelaramosdesigner@gmail.com>
2019-05-28 22:09:24 -0300

    node-js

--- dia-1/servidor/server.js
+++ dia-1/servidor/server.js
@@ -1,1 +1,20 @@
 const http = require ('http')
+
+const servidor = http.createServer(function(request, response){
+    if(request.url ==='/'){
+        response.end('to com sono')
+    }else if (request.url=== '/comidas'){
+      response.writeHead(200,{"Content-Type": "text/html;charset=utf-8"})
+        response.end ("<h1>respostão diferente</h1>")
+        if(request.method=== 'GET'){
+            response.writeHead(200,{
+                "Content-Type": "text/html;charset=utf-8"
+            })
+            response.end('resposta do GET')
+        }
+
+    }
+})
+
+servidor.listen(3000)
+console.log('servidor rodando')
