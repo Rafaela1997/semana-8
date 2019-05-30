d7360660e4efbcf53dfd3e892b9bc7c96f1a7963
rafaela1997 <rafaelaramosdesigner@gmail.com>
2019-05-29 22:14:25 -0300

    deu certo

--- dia-1/servidor/server.js
+++ dia-1/servidor/server.js
@@ -1,18 +1,40 @@
 const http = require ('http')
 
+const comidas = {
+    comidas:[
+        {
+    "nome" : "fricase",
+    "descrição":"frango com creme de leite",
+    "imagem":"https://img.itdg.com.br/images/recipes/000/010/254/294238/294238_original.jpg"
+},
+
+    {"nome" : "brigadeiro",
+     "descrição":"doce de chocolate",
+    "imagem":"https://www.comercialmartini.com.br/wp-content/uploads/2016/09/brigadeiro.png"
+}
+]
+}
+
 const servidor = http.createServer(function(request, response){
     if(request.url ==='/'){
         response.end('to com sono')
     }else if (request.url=== '/comidas'){
       response.writeHead(200,{"Content-Type": "text/html;charset=utf-8"})
-        response.end ("<h1>respostão diferente</h1>")
+        
         if(request.method=== 'GET'){
             response.writeHead(200,{
+                "Content-Type": "application/json",
+                "access-Control-Allow-Origin":"*"
+            })
+
+            response.write(JSON.stringify(comidas))
+            response.end()
+        }else if (request.method === 'POST'){
+            response.writeHead(201,{
                 "Content-Type": "text/html;charset=utf-8"
             })
-            response.end('resposta do GET')
+            response.end("ainda estou com sono")
         }
-
     }
 })
 
