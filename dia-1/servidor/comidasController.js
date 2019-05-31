a9a63e42a8debc1d1da01c6a8e07a404f2801988
rafaela1997 <rafaelaramosdesigner@gmail.com>
2019-05-30 21:54:28 -0300

    get com express

---
+++ dia-1/servidor/comidasController.js
@@ -0,0 +1,21 @@
+const comidas = {
+    comidas:[
+        {
+    "nome" : "fricase",
+    "descrição":"frango com creme de leite",
+    "imagem":"https://img.itdg.com.br/images/recipes/000/010/254/294238/294238_original.jpg"
+},
+
+    {"nome" : "brigadeiro",
+     "descrição":"doce de chocolate",
+    "imagem":"https://www.comercialmartini.com.br/wp-content/uploads/2016/09/brigadeiro.png"
+}
+]
+}
+const getAll = () => {
+    return comidas
+}
+
+module.exports = {
+getAll
+}
--- dia-1/servidor/server.js
+++ dia-1/servidor/server.js
@@ -1,42 +1,38 @@
-const http = require ('http')
+const express = require('express')
+const cors = require ('cors')
+const controller = require('./comidasController')
 
-const comidas = {
-    comidas:[
-        {
-    "nome" : "fricase",
-    "descrição":"frango com creme de leite",
-    "imagem":"https://img.itdg.com.br/images/recipes/000/010/254/294238/294238_original.jpg"
-},
+controller.getAll()
 
-    {"nome" : "brigadeiro",
-     "descrição":"doce de chocolate",
-    "imagem":"https://www.comercialmartini.com.br/wp-content/uploads/2016/09/brigadeiro.png"
-}
-]
-}
 
-const servidor = http.createServer(function(request, response){
-    if(request.url ==='/'){
-        response.end('to com sono')
-    }else if (request.url=== '/comidas'){
-      response.writeHead(200,{"Content-Type": "text/html;charset=utf-8"})
+// const servidor = http.createServer(function(request, response){
+//     if(request.url ==='/'){
+//         response.end('to com sono')
+//     }else if (request.url=== '/comidas'){
+//       response.writeHead(200,{"Content-Type": "text/html;charset=utf-8"})
         
-        if(request.method=== 'GET'){
-            response.writeHead(200,{
-                "Content-Type": "application/json",
-                "access-Control-Allow-Origin":"*"
-            })
+//         if(request.method=== 'GET'){
+//             response.writeHead(200,{
+//                 "Content-Type": "application/json",
+//                 "access-Control-Allow-Origin":"*"
+//             })
 
-            response.write(JSON.stringify(comidas))
-            response.end()
-        }else if (request.method === 'POST'){
-            response.writeHead(201,{
-                "Content-Type": "text/html;charset=utf-8"
-            })
-            response.end("ainda estou com sono")
-        }
-    }
+//             response.write(JSON.stringify(comidas))
+//             response.end()
+//         }else if (request.method === 'POST'){
+//             response.writeHead(201,{
+//                 "Content-Type": "text/html;charset=utf-8"
+//             })
+//             response.end("ainda estou com sono")
+//         }
+//     }
+// })
+const servidor = express()
+servidor.use(cors())
+
+servidor.get("/comidas", (request, response)=>{
+    response.send(controller.getAll())
+
 })
-
 servidor.listen(3000)
 console.log('servidor rodando')
