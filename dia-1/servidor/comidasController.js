93ec05a216beaac61978818966c62ea525543ab7
rafaela1997 <rafaelaramosdesigner@gmail.com>
2019-05-31 22:35:07 -0300

    fiz o get, post e o delete

--- dia-1/servidor/comidasController.js
+++ dia-1/servidor/comidasController.js
@@ -1,21 +1,23 @@
 const comidas = {
-    comidas:[
-        {
-    "nome" : "fricase",
-    "descrição":"frango com creme de leite",
-    "imagem":"https://img.itdg.com.br/images/recipes/000/010/254/294238/294238_original.jpg"
-},
-
-    {"nome" : "brigadeiro",
-     "descrição":"doce de chocolate",
-    "imagem":"https://www.comercialmartini.com.br/wp-content/uploads/2016/09/brigadeiro.png"
-}
-]
+    comidasFavoritas:[]
 }
 const getAll = () => {
     return comidas
 }
 
+const add = (comida) => {
+   comida.id = Math.random().toString(36).substr(-8)
+    getAll().comidasFavoritas.push(comida)
+}//gera um Id
+
+const remove =  (id) => {
+    let comidasRestantes = getAll()
+     getAll().comidasFavoritas=comidasRestantes.comidasFavoritas.filter((comida) => {
+         return comida.id !== id
+     })
+}
 module.exports = {
-getAll
+getAll,
+add,
+remove
 }
--- dia-1/servidor/server.js
+++ dia-1/servidor/server.js
@@ -1,38 +1,26 @@
 const express = require('express')
 const cors = require ('cors')
 const controller = require('./comidasController')
+const bodyParse = require("body-parser")
 
-controller.getAll()
-
-
-// const servidor = http.createServer(function(request, response){
-//     if(request.url ==='/'){
-//         response.end('to com sono')
-//     }else if (request.url=== '/comidas'){
-//       response.writeHead(200,{"Content-Type": "text/html;charset=utf-8"})
-        
-//         if(request.method=== 'GET'){
-//             response.writeHead(200,{
-//                 "Content-Type": "application/json",
-//                 "access-Control-Allow-Origin":"*"
-//             })
-
-//             response.write(JSON.stringify(comidas))
-//             response.end()
-//         }else if (request.method === 'POST'){
-//             response.writeHead(201,{
-//                 "Content-Type": "text/html;charset=utf-8"
-//             })
-//             response.end("ainda estou com sono")
-//         }
-//     }
-// })
 const servidor = express()
 servidor.use(cors())
+servidor.use(bodyParse.json())
 
 servidor.get("/comidas", (request, response)=>{
     response.send(controller.getAll())
+    
+})
+servidor.post("/comidas",(request,response)=>{
+    controller.add(request.body)
+    response.send(201)
 
 })
+
+servidor.delete("/comidas/:id", (request, response)=> {
+    controller.remove(request.params.id)
+    response.sendStatus(204)
+})
+
 servidor.listen(3000)
 console.log('servidor rodando')
